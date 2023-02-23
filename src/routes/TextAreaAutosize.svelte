ac07cad98a5867c59ced4532d98f452a8d9343e5
Michi Palazzo <palazzomichi@gmail.com>
2023-02-23 18:26:17 +0100

    Init WriteField

---
+++ src/routes/TextAreaAutosize.svelte
@@ -0,0 +1,40 @@
+<script>
+	export let value = '';
+	export let minRows = 1;
+	export let maxRows;
+	export let placeholder;
+	export let clazz;
+
+	$: minHeight = `${1 + minRows * 1.2}em`;
+	$: maxHeight = maxRows ? `${1 + maxRows * 1.2}em` : `auto`;
+</script>
+
+<div class="container">
+	<pre aria-hidden="true" style="min-height: {minHeight}; max-height: {maxHeight}">{value +
+			'\n'}</pre>
+
+	<textarea class={clazz} {placeholder} bind:value />
+</div>
+
+<style>
+	.container {
+		position: relative;
+	}
+
+	pre,
+	textarea {
+		padding: 0.5em;
+		box-sizing: border-box;
+		line-height: 1.2;
+		overflow: hidden;
+	}
+
+	textarea {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		padding: 0;
+		resize: none;
+	}
+</style>
---
+++ src/routes/WriteField.svelte
@@ -0,0 +1,81 @@
+<script>
+	import TextAreaAutosize from './TextAreaAutosize.svelte';
+	import BellPlus from 'svelte-material-icons/BellPlusOutline.svelte';
+	import AccountPlus from 'svelte-material-icons/AccountPlusOutline.svelte';
+	import PaletteOutline from 'svelte-material-icons/PaletteOutline.svelte';
+	import ImageOutline from 'svelte-material-icons/ImageOutline.svelte';
+	import Archive from 'svelte-material-icons/ArchiveOutline.svelte';
+	import DotsVertical from 'svelte-material-icons/DotsVertical.svelte';
+	import ArrowULeftTop from 'svelte-material-icons/ArrowULeftTop.svelte';
+	import ArrowURightTop from 'svelte-material-icons/ArrowURightTop.svelte';
+
+	let focused = false;
+
+	function init(title) {
+		title.focus();
+	}
+</script>
+
+<div
+	class="self-center w-[600px] px-4 py-3 shadow-md shadow-[#00000050] rounded-md my-7 flex flex-col gap-6"
+>
+	{#if focused}
+		<input class="outline-none placeholder-[#4c4c4c]" type="text" placeholder="Titolo" use:init />
+		<TextAreaAutosize
+			clazz="outline-none placeholder-[#4c4c4c] resize-none m-0 min-h-full h-auto text-sm"
+			placeholder="Scrivi una nota..."
+		/>
+		<div class="flex justify-between items-center">
+			<div class="ml-[-6px] flex items-center gap-4">
+				<BellPlus
+					class="box-content p-2 cursor-pointer rounded-full hover:bg-[#e3e5e6]"
+					size="16"
+					color="#202124"
+				/>
+				<AccountPlus
+					class="box-content p-2 cursor-pointer rounded-full hover:bg-[#e3e5e6]"
+					size="16"
+					color="#202124"
+				/>
+				<PaletteOutline
+					class="box-content p-2 cursor-pointer rounded-full hover:bg-[#e3e5e6]"
+					size="16"
+					color="#202124"
+				/>
+				<ImageOutline
+					class="box-content p-2 cursor-pointer rounded-full hover:bg-[#e3e5e6]"
+					size="16"
+					color="#202124"
+				/>
+				<Archive
+					class="box-content p-2 cursor-pointer rounded-full hover:bg-[#e3e5e6]"
+					size="16"
+					color="#202124"
+				/>
+				<DotsVertical
+					class="box-content p-2 cursor-pointer rounded-full hover:bg-[#e3e5e6]"
+					size="16"
+					color="#202124"
+				/>
+				<ArrowULeftTop
+					class="box-content p-2 cursor-pointer rounded-full hover:bg-[#e3e5e6]"
+					size="16"
+					color="#202124"
+				/>
+				<ArrowURightTop
+					class="box-content p-2 cursor-pointer rounded-full hover:bg-[#e3e5e6]"
+					size="16"
+					color="#202124"
+				/>
+			</div>
+			<div>Chiudi</div>
+		</div>
+	{:else}
+		<input
+			class="outline-none placeholder-[#4c4c4c]"
+			type="text"
+			placeholder="Scrivi una nota..."
+			on:focus={() => (focused = true)}
+		/>
+	{/if}
+</div>
